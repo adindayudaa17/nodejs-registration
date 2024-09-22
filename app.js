460b84f8d4da3e7df28a4eb0899a02baa01b01ee
yuliaadinda <adindayudaa17@gmail.com>
2024-09-22 10:15:01 +0700

    add localhost url

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-require('dotenv').config();
-const express = require('express');
-const userRoutes = require('./routes/userRoutes');
-const errorHandler = require('./middlewares/errorMiddleware');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const userRoutes = require("./routes/userRoutes");
+const errorHandler = require("./middlewares/errorMiddleware");
+const cors = require("cors");
 
 const app = express();
 
-app.use(cors({ origin: process.env.FE_URL }));
-
+const allowed_url = process.env.FE_URL.split(",");
+app.use(cors({ origin: allowed_url }));
 app.use((req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
+  console.log(`${req.method} ${req.url}`);
+  next();
 });
 
 // Middleware untuk parsing JSON
 app.use(express.json());
 
 // Rute API
-app.use('/api/user', userRoutes);
+app.use("/api/user", userRoutes);
 
 // Middleware untuk menangani error
 app.use(errorHandler);
